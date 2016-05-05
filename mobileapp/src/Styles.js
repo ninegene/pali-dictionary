91597a088ea93c5c95aa7828bf3ac432ef7a559e
aung <aung@macdev.local>
2016-05-05 01:08:20 -0700

    initial Search box and ListView

---
+++ mobileapp/src/Styles.js
@@ -0,0 +1,13 @@
+let Styles = {
+  primaryColor: '#163C48',
+  secondaryColor: '#163C48',
+  primaryBackground: '#f2f2f2',
+  secondaryBackground: '#e1e1e1',
+  xsFont: 12,
+  smFont: 16,
+  mdFont: 20,
+  lgFont: 24,
+  xlFont: 30,
+};
+
+export default Styles;
---
+++ mobileapp/src/components/App.js
@@ -0,0 +1,139 @@
+import React from "react-native";
+import Styles from "../Styles";
+
+const {
+  View,
+  Text,
+  TextInput,
+  ListView,
+  RecyclerViewBackedScrollView,
+  TouchableHighlight,
+} = React;
+
+// See: https://facebook.github.io/react/docs/reusable-components.html#prop-validation
+const propTypes = {
+  instructions: React.PropTypes.string,
+};
+
+function genRows(pressedRows = {}) {
+  let rows = [];
+  for (let i = 0; i < 100; i++) {
+    rows.push({mm: 'mm ' + i, pali: 'Row ' + i + (pressedRows[i] ? ' (pressed)': '')});
+  }
+  return rows;
+}
+
+class App extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    this.state = {
+      q: '',
+      dataSource: ds.cloneWithRows(genRows()),
+    };
+
+    this.pressRows = {};
+  }
+
+  handleSubmitSearch() {
+    console.log('handleSubmitSearch', this.state.q);
+  }
+
+  handleRowPress(rowID) {
+    console.log('handleRowPress', rowID);
+    this.pressRows[rowID] = !this.pressRows[rowID];
+    this.setState({dataSource: this.state.dataSource.cloneWithRows(genRows(this.pressRows))});
+  }
+
+  renderRows(rowData, sectionID, rowID) {
+    return (
+      <TouchableHighlight onPress={this.handleRowPress.bind(this, rowID)}>
+        <View>
+          <View style={styles.row}>
+            <Text style={styles.rowTitle}>
+              {rowData.mm}
+            </Text>
+            <Text style={styles.rowText}>
+              {rowData.pali}
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <View style={styles.searchRow}>
+          <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="always"
+            onChangeText={text => {
+           this.setState({q: text});
+           }}
+            placeholder="Search..."
+            style={styles.searchTextInput}
+            value={this.state.q}
+            onSubmitEditing={this.handleSubmitSearch.bind(this)}
+          />
+        </View>
+        <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRows.bind(this)}
+            renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+            renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={styles.separator} />}
+          />
+      </View>
+    );
+  }
+}
+
+// See: https://facebook.github.io/react-native/docs/flexbox.html
+const styles = React.StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: Styles.primaryBackground,
+  },
+  searchRow: {
+    backgroundColor: '#eeeeee',
+    padding: 10,
+  },
+  searchTextInput: {
+    backgroundColor: 'white',
+    borderColor: '#cccccc',
+    borderRadius: 3,
+    borderWidth: 1,
+    paddingLeft: 8,
+    height: 35,
+  },
+
+  row: {
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#F6F6F6',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+  rowContent: {
+
+  },
+  rowTitle: {
+    fontSize: Styles.mdFont,
+  },
+  rowText: {
+    fontSize: Styles.smFont,
+  },
+
+});
+
+App.propTypes = propTypes;
+
+export default App;
